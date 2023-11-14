fc357e2abd1773eb749ef12fe6f73dd7a88c2022
Syamily <syamilygetsmail@gmail.com>
2023-11-14 07:54:06 +0530

    latestone

---
+++ Details.java
@@ -0,0 +1,15 @@
+class Details {
+    public static void main(String[] args) {
+        String name = "Mohan";
+        String address = "Phase one Technopark";
+        String location = "Trivandrum";
+        int age = 60;
+        String profession = "Tester";
+
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(location);
+        System.out.println(age);
+        System.out.println(profession);
+    }
+}
