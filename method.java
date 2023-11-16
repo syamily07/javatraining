2fab82b80e868b753dd75b22f20f12bb70c7da19
Syamily <syamilygetsmail@gmail.com>
2023-11-16 07:59:58 +0530

    methodeg

---
+++ method.java
@@ -0,0 +1,30 @@
+class method {
+    public static void main(String[] args) {
+        String name = "Mohan";
+        String address = "Phase one Technopark";
+        String location = "Trivandrum";
+        int age = 60;
+        String profession = "Tester";
+
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(location);
+        System.out.println(age);
+        System.out.println(profession);
+        method1();
+        method2();
+        method3();
+    }
+    public static void method1(){
+         String address = "Phase one Technopark";
+         System.out.println(address);
+    }
+     public static void method2(){
+        String location = "Trivandrum";
+        System.out.println(location);
+     }
+public static void method3(){
+    String profession = "Tester";
+    System.out.println(profession);
+}
+}
