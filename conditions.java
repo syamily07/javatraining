11c8293f343888b054b03b86cb627ada4db1a52e
Syamily <syamilygetsmail@gmail.com>
2023-11-16 22:49:09 +0530

    Conditionseg

---
+++ conditions.java
@@ -0,0 +1,21 @@
+class conditions {
+    public static void main(String[] args) {
+int balance =99;
+if(balance>=100)
+{
+    System.out.println("Your balance is above 100");
+
+   if (balance >= 100 && balance <= 200)
+    {
+        System.out.println("Your balance is between 100 and 200");
+    }
+        else 
+        {
+            System.out.println("Your balance is above 200");
+    }}
+    else{
+        System.out.println("Your balance is below 100");
+    }
+
+}
+}
