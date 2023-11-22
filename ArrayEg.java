424dbffcfcd3bdefc46d7bd945b0d7ccdfe50660
syamily <syamilygetsmail@gmail.com>
2023-11-22 17:59:48 +0530

    Array program

---
+++ ArrayEg.java
@@ -0,0 +1,14 @@
+
+class ArrayEg {
+    public static void main(String[] args){
+    Integer[][] regNum ={{1,2,3,4},{5,6,7}};
+    for(int i=0; i<regNum.length;i++){
+    
+    for (int j=0; j<regNum[i].length;j++){
+    
+
+        System.out.printf("%s",regNum[i][j]);
+        
+    } System.out.printf("\n");
+}
+    }}
