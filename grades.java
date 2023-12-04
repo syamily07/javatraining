02909458d8f9ad88e4bf95ceb86cdebb3baeb521
syamily <syamilygetsmail@gmail.com>
2023-12-04 16:23:42 +0530

    assignment using constructor

---
+++ grades.java
@@ -0,0 +1,21 @@
+public class grades {
+    public static void main(String[] args){
+       
+
+       markscalculator Anna= new markscalculator(20, 30, 20); 
+        markscalculator Mery= new markscalculator(20, 20, 20); 
+        System.out.printf("The total marks of Anna is : %d%n", Anna.gettotalMarks());
+        System.out.printf("The grade of Anna is %s%n", Anna.gradecalculated());
+        System.out.printf("The total marks of Mery is %d: %n", Mery.gettotalMarks());
+        System.out.printf("The grade of Mery is %s: %n", Mery.gradecalculated());
+    }
+
+    
+    }
+
+   
+    
+
+   
+    
+
