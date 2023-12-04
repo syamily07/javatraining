1525af9c4043b80d0e23da7d523f3bc958583d6e
syamily <syamilygetsmail@gmail.com>
2023-12-04 17:32:25 +0530

    assignment using constructor on average of 3 integer numbers

---
+++ averagesub.java
@@ -0,0 +1,23 @@
+public class averagesub {
+public int firstInteger;
+public int secondInteger;
+public int thirdInteger;
+public int count;
+
+public averagesub(int firstInteger, int secondInteger, int thirdInteger){
+    this.firstInteger=firstInteger;
+    this.secondInteger=secondInteger;
+    this.thirdInteger=thirdInteger;
+}
+public int sumValue(){
+    return firstInteger+secondInteger+thirdInteger;
+}
+public int countValue(){
+return 3;
+}
+public int averageValue(){
+    int average=sumValue()/countValue();
+    return average;
+
+}   
+} 
