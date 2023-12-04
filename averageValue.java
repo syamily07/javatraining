d5fb131101c40dbd263cb943cdd107d0e980bede
syamily07 <150066267+syamily07@users.noreply.github.com>
2023-12-04 17:30:32 +0530

    Update averageValue.java

--- averageValue.java
+++ averageValue.java
@@ -2,16 +2,9 @@ public class averageValue {
 
     public static void main(String[] args){
         averagesub value = new averagesub(2,2,2);
-        //averagesub value2 = new averagesub(3,3,3);
-        ///averagesub value3 = new averagesub(4,4,4);
         System.out.printf("The sum of three values:  %s%n ",value.sumValue());
         System.out.printf("The average of three values:  %s",value.averageValue());
-        //System.out.printf("The average of three values is %s",value2.averageValue());
-        //System.out.printf("The average of three values is %s",value3.averageValue());
-
-    }
-
-    
-    }
+        }
+}
     
 
